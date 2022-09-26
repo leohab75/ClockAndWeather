ed71870f74ff8f46d10daa56c77fda0d4c3f6098
leohab75 <leohab@ya.ru>
2022-09-27 01:56:27 +0300

    main

--- scripts/rest_weather.sh
+++ scripts/rest_weather.sh
@@ -8,31 +8,31 @@ fi
 curl wttr.in/?format=j1 -s -o $HOME/.cache/weather.json
 
 #сегодня
-code=$(cat weather.json | jq -r '.weather[0].hourly[2].weatherCode')
+code=$(cat $HOME/.cache/weather.json | jq -r '.weather[0].hourly[2].weatherCode')
 curl https://cdn.weatherapi.com/weather/64x64/day/$code.png -s -o ~/.cache/weather/1.1.png
 
-code=$(cat weather.json | jq -r '.weather[0].hourly[4].weatherCode')
+code=$(cat $HOME/.cache/weather.json | jq -r '.weather[0].hourly[4].weatherCode')
 curl https://cdn.weatherapi.com/weather/64x64/day/$code.png -s -o ~/.cache/weather/1.2.png
 
-code=$(cat weather.json | jq -r '.weather[0].hourly[4].weatherCode')
+code=$(cat $HOME/.cache/weather.json | jq -r '.weather[0].hourly[4].weatherCode')
 curl https://cdn.weatherapi.com/weather/64x64/day/$code.png -s -o ~/.cache/weather/1.3.png
 
 #завтра
-code=$(cat weather.json | jq -r '.weather[1].hourly[2].weatherCode')
+code=$(cat $HOME/.cache/weather.json | jq -r '.weather[1].hourly[2].weatherCode')
 curl https://cdn.weatherapi.com/weather/64x64/day/$code.png -s -o ~/.cache/weather/2.1.png
 
-code=$(cat weather.json | jq -r '.weather[1].hourly[4].weatherCode')
+code=$(cat $HOME/.cache/weather.json | jq -r '.weather[1].hourly[4].weatherCode')
 curl https://cdn.weatherapi.com/weather/64x64/day/$code.png -s -o ~/.cache/weather/2.2.png
 
-code=$(cat weather.json | jq -r '.weather[1].hourly[6].weatherCode')
+code=$(cat $HOME/.cache/weather.json | jq -r '.weather[1].hourly[6].weatherCode')
 curl https://cdn.weatherapi.com/weather/64x64/day/$code.png -s -o ~/.cache/weather/2.3.png
 
 #послезавтра
-code=$(cat weather.json | jq -r '.weather[2].hourly[2].weatherCode')
+code=$(cat $HOME/.cache/weather.json | jq -r '.weather[2].hourly[2].weatherCode')
 curl https://cdn.weatherapi.com/weather/64x64/day/$code.png -s -o ~/.cache/weather/3.1.png
 
-code=$(cat weather.json | jq -r '.weather[2].hourly[4].weatherCode')
+code=$(cat $HOME/.cache/weather.json | jq -r '.weather[2].hourly[4].weatherCode')
 curl https://cdn.weatherapi.com/weather/64x64/day/$code.png -s -o ~/.cache/weather/3.2.png
 
-code=$(cat weather.json | jq -r '.weather[2].hourly[6].weatherCode')
+code=$(cat $HOME/.cache/weather.json | jq -r '.weather[2].hourly[6].weatherCode')
 curl https://cdn.weatherapi.com/weather/64x64/day/$code.png -s -o ~/.cache/weather/3.3.png
