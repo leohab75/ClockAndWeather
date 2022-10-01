b38a675c223201845240efce580d923f5f69476d
leohab75 <leohab@ya.ru>
2022-10-01 03:41:04 +0300

    main

--- scripts/translate.sh
+++ scripts/translate.sh
@@ -1,10 +1,12 @@
 #!/bin/env bash
 
-if [[ ! -f trans ]]; then
+if [[ ! -f  $HOME/.conky/ClockAndWeather/scripts/trans ]]; then
     wget git.io/trans -o $HOME/.conky/ClockAndWeather/scripts/
     chmod +x $HOME/.conky/ClockAndWeather/scripts/trans
 fi
 
 weatherDesc=$(cat $HOME/.cache/weather.txt | cut -f 2 -d ":")
 
-echo $weatherDesc | ./trans -s=en -t=ru -show-dictionary=n -show-translation-phonetics=n -show-original=n -show-languages=n -show-prompt-message=n -show-original=n -show-original-dictionary=n -show-original=n -show-alternatives=n -no-warn
+trans=$(echo $weatherDesc | ./trans -s=en -t=ru -show-dictionary=n -show-translation-phonetics=n -show-original=n -show-languages=n -show-prompt-message=n -show-original=n -show-original-dictionary=n -show-original=n -show-alternatives=n -no-warn)
+
+echo -e "$trans"
